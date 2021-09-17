ed1df6dcd6aba4dd20646103d3aebd8b3f132ab9
florsc <f.schimmel@yahoo.de>
2021-09-17 14:31:42 +0200

    remove build dir

--- build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/florian/CLionProjects/tumorSimulation/build/tests[1]_tests.cmake")
-  include("/home/florian/CLionProjects/tumorSimulation/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
